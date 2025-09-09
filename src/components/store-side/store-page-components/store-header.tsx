14bd67c933005611de0ff88911a7df462d7b7509
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-09-08 23:38:19 -0300

    header store usando contexto

--- src/components/store-side/store-page-components/store-header.tsx
+++ src/components/store-side/store-page-components/store-header.tsx
@@ -1,59 +1,72 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-//import { OpeningHour } from "../../../types/types-schedules.d";
+import { useRestaurantData } from "../../../context/restaurant-data/restaurant-data-context";
+import { useManageMenu } from "../../../context/manage-menu/manage-menu-context";
+import { UpdateStoreDataForm } from "../forms/form-update-data-store";
+import { UpdateSchedulesForm } from "../forms/form-update-schedules";
 import { Logo } from "./store-logo";
 import { GoHomeFill } from "react-icons/go";
 import { FaGear } from "react-icons/fa6";
 import { TbClockHour2Filled } from "react-icons/tb";
-import { useManageMenu } from "../../../context/manage-menu/manage-menu-context";
-import { useRestaurantData } from "../../../context/restaurant-data/restaurant-data-context";
 
 export const Header = () => {
 
     const { restaurantData } = useRestaurantData();
     const { tempBackgroundColor } = useManageMenu();
 
+    const [openFormUpdateDataStore, setOpenFormUpdateDataStore] = useState(false);
+    const [openFormUpdateSchedules, setOpenFormUpdateSchedules] = useState(false);
+
     return (
-        <header
-            className={`w-screen max-h-[387px] px-[5%] lg:px-[15%] py-4 xl:py-4 sm:px-6 flex flex-col sm:flex-row items-center justify-between shadow-md ms:sticky top-0 ${tempBackgroundColor === 'white' ? 'text-black bg-white' : 'text-white bg-black border-[1px] border-zinc-800'}`}
-            style={{ zIndex: 5 }}
-        >
-            <div className="flex flex-col ms:flex-row items-center gap-3.5">
-                <Logo />
-                <div className="mx-1.5 text-center sm:text-start">
-                    <h5 className="text-md font-bold mb-1">
-                        {restaurantData?.user.restaurantName && restaurantData.user.restaurantName.length > 25
-                            ? restaurantData?.user.restaurantName.slice(0, 25) + '...'
-                            : restaurantData?.user.restaurantName}
-                    </h5>
-                    <div className="flex flex-col items-center sm:items-start justify-center gap-1 ms:justify-start flex-shrink-0">
-                        <button
-                            style={{ fontSize: '14px' }}
-                            // onClick={openFormUpdateDataStore}
-                            title="Configurar dados da loja"
-                            className="rounded-full bg-gray-400 text-black border-1 flex items-center justify-center px-3 gap-2 cursor-pointer hover:scale-105 transition-all duration-200"
-                        >
-                            <FaGear />
-                            <span>Configurar Loja</span>
-                        </button>
-                        <button
-                            style={{ fontSize: '14px' }}
-                            //onClick={openFormUpdateSchedules}
-                            title="Configurar dados da loja"
-                            className="rounded-full bg-gray-400 text-black border-1 flex items-center justify-center px-3 gap-2 cursor-pointer hover:scale-105 transition-all duration-200"
-                        >
-                            <TbClockHour2Filled />
-                            <span>Editar Horários</span>
-                        </button>
+        <>
+            <header
+                className={`w-screen max-h-[387px] px-[5%] lg:px-[15%] py-4 xl:py-4 sm:px-6 flex flex-col sm:flex-row items-center justify-between shadow-md ms:sticky top-0 ${tempBackgroundColor === 'white' ? 'text-black bg-white' : 'text-white bg-black border-[1px] border-zinc-800'}`}
+                style={{ zIndex: 5 }}
+            >
+                <div className="flex flex-col ms:flex-row items-center gap-3.5">
+                    <Logo />
+                    <div className="mx-1.5 text-center sm:text-start">
+                        <h5 className="text-md font-bold mb-1">
+                            {restaurantData?.user.restaurantName && restaurantData.user.restaurantName.length > 25
+                                ? restaurantData?.user.restaurantName.slice(0, 25) + '...'
+                                : restaurantData?.user.restaurantName}
+                        </h5>
+                        <div className="flex flex-col items-center sm:items-start justify-center gap-1 ms:justify-start flex-shrink-0">
+                            <button
+                                style={{ fontSize: '14px' }}
+                                onClick={() => setOpenFormUpdateDataStore(true)}
+                                title="Configurar dados da loja"
+                                className="rounded-full bg-gray-400 text-black border-1 flex items-center justify-center px-3 gap-2 cursor-pointer hover:scale-105 transition-all duration-200"
+                            >
+                                <FaGear />
+                                <span>Configurar Loja</span>
+                            </button>
+                            <button
+                                style={{ fontSize: '14px' }}
+                                onClick={() => setOpenFormUpdateSchedules(true)}
+                                title="Configurar dados da loja"
+                                className="rounded-full bg-gray-400 text-black border-1 flex items-center justify-center px-3 gap-2 cursor-pointer hover:scale-105 transition-all duration-200"
+                            >
+                                <TbClockHour2Filled />
+                                <span>Editar Horários</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <Link
-                to="/"
-                className="p-2 mt-3 sm:mt-0 rounded-full bg-primary text-black flex items-center gap-2 z-50 hover:scale-105 transition-all duration-200"
-            >
-                <GoHomeFill />
-                voltar
-            </Link>
-        </header>
+                <Link
+                    to="/"
+                    className="p-2 mt-3 sm:mt-0 rounded-full bg-primary text-black flex items-center gap-2 z-50 hover:scale-105 transition-all duration-200"
+                >
+                    <GoHomeFill />
+                    voltar
+                </Link>
+            </header>
+            {openFormUpdateDataStore && (
+                <UpdateStoreDataForm onClose={() => setOpenFormUpdateDataStore(false)} />
+            )}
+            {openFormUpdateSchedules && (
+                <UpdateSchedulesForm onClose={() => setOpenFormUpdateSchedules(false)} />
+            )}
+        </>
     );
 };
