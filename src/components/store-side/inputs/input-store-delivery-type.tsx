9a7aaecb7dbcc9497e762398e6b28e9d141d286d
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-09-10 08:51:30 -0300

    inout de delivery type com cores da loja

--- src/components/store-side/inputs/input-store-delivery-type.tsx
+++ src/components/store-side/inputs/input-store-delivery-type.tsx
@@ -4,12 +4,13 @@ export const CheckboxDeliveryTypesInput = ({
     register,
     errors,
 }: InputDeliveryTypeProps) => (
-    <div className="flex flex-col gap-1 pl-2">
+    <div className="flex flex-col gap-1">
         <span className="font-bold flex">Tipos de entrega:  {<span className="flex-1 text-end text-red-600">{errors.delivery?.message || errors.pickup?.message}</span>}</span>
         <div className="flex gap-4 ml-4 dark:text-gray-300">
             <input
                 type="checkbox"
                 id="delivery"
+                className="accent-primary"
                 {...register("delivery", {
                     validate: (value, formValues) =>
                         value || formValues.pickup || "obrigatório"
@@ -21,6 +22,7 @@ export const CheckboxDeliveryTypesInput = ({
             <input
                 type="checkbox"
                 id="pickup"
+                className="accent-primary"
                 {...register("pickup", {
                     validate: (value, formValues) =>
                         value || formValues.delivery || "obrigatório"
