df983fe554227fcc6ab6d6f59326038aa4153aab
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-09-13 11:19:18 -0300

    criado componente de loading interno

--- src/components/component-loading.tsx
+++ src/components/component-loading.tsx
@@ -3,7 +3,15 @@ export const LoadingComponent = () => {
         <div className="fixed inset-0 w-screen h-screen flex items-center justify-center bg-opacity-0 z-50">
             <div className="flex flex-col items-center justify-center">
                 <div className="animate-spin rounded-full border-7 border-primary border-t-transparent w-18 h-18 mb-4"></div>
-                <span className="dark:text-white text-black text-2xl">Carregando</span>
+            </div>
+        </div>
+    );
+};
+export const LoadingComponentInternal = () => {
+    return (
+        <div className="flex items-center justify-center bg-opacity-0 z-50">
+            <div className="flex flex-col items-center justify-center">
+                <div className="animate-spin rounded-full border-7 border-primary border-t-transparent w-18 h-18 mb-4"></div>
             </div>
         </div>
     );
